d124b8e9404483f710bbe5a5c170141967cd15dc
kvker <kk@kvker.com>
2021-10-06 15:33:07 +0800

    feat: add ejs complier

--- gruntfile.js
+++ gruntfile.js
@@ -1,6 +1,33 @@
 module.exports = function (grunt) {
+  // 加载包含 "uglify" 任务的插件。
+  grunt.loadNpmTasks('grunt-ejs')
+  grunt.loadNpmTasks('grunt-contrib-watch')
+  grunt.loadNpmTasks('grunt-contrib-uglify')
+  grunt.loadNpmTasks('grunt-contrib-htmlmin')
+  grunt.loadNpmTasks('grunt-contrib-cssmin')
+  grunt.loadNpmTasks('grunt-contrib-copy')
+
+  // 默认被执行的任务列表。
+  grunt.registerTask('default', ['ejs', 'uglify', 'cssmin', 'htmlmin', 'copy'])
+
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    watch: {
+      files: ['src/ejs/**/*.ejs'],
+      tasks: ['ejs:build'],
+    },
+    ejs: {
+      options: {
+        banner: '/*! <%= pkg.description %> <%= pkg.author %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
+      },
+      build: {
+        expand: true,
+        cwd: 'src/ejs',
+        src: ['**/*.ejs', '!common/**/*.ejs'],
+        dest: 'src/',
+        ext: '.html',
+      },
+    },
     uglify: {
       options: {
         banner: '/*! <%= pkg.description %> <%= pkg.author %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
@@ -57,13 +84,4 @@ module.exports = function (grunt) {
       },
     },
   })
-
-  // 加载包含 "uglify" 任务的插件。
-  grunt.loadNpmTasks('grunt-contrib-uglify')
-  grunt.loadNpmTasks('grunt-contrib-htmlmin')
-  grunt.loadNpmTasks('grunt-contrib-cssmin')
-  grunt.loadNpmTasks('grunt-contrib-copy')
-
-  // 默认被执行的任务列表。
-  grunt.registerTask('default', ['uglify', 'cssmin', 'htmlmin', 'copy'])
 }
