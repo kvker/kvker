c7bc1b2e9167aff0ee2bd8eef5b3cdfefd4433f8
kvker <kk@kvker.com>
2021-10-06 22:23:41 +0800

    feat: 删除不用的库文件

--- src/static/js/theme.js
+++
@@ -1,8 +0,0 @@
-document.body.insertAdjacentHTML('afterbegin', `<nav class="flex jcsb top-nav">
-<div class="nav-side"></div>
-<div class="flex jcc aic">
-  <a class="nav-item" href="/">首页</a>
-  <a class="nav-item" href="/tool.html">工具</a>
-</div>
-<div id="account" class="nav-side">账号</div>
-</nav>`)
