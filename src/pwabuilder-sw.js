6561e7061e65b0f0e212f295dce86c2c6dc9c0ac
kvker <kk@kvker.com>
2021-10-08 22:36:37 +0800

    feat: 优化pwa配置

--- src/pwabuilder-sw.js
+++ src/pwabuilder-sw.js
@@ -1,43 +1,55 @@
-let cacheName = 'kvker-1' // 决定是否更新本地资源，每次要更新记得替换
+// This is the service worker with the combined offline experience (Offline page + Offline copy of pages)
 
-let cacheList = [
-  // 需要保存本地资源列表，支持*匹配
-  '/',
-  '/index.html',
-]
+const CACHE = 'kvker-1' // 决定是否更新本地资源，每次要更新记得替换
 
-// Service Worker 注册完成事件，写入缓存
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches
-      .open(cacheName)
-      .then((cache) => cache.addAll(cacheList))
-      .then(() => self.skipWaiting())
-  )
-})
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-// Service Worker 启动事件，处理更新缓存
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    Promise.all(
-      [caches.keys().then((keys) =>
-        keys.map((key) => {
-          if (key !== cacheName) {
-            return caches.delete(key)
-          } else {
-            return Promise.resolve()
-          }
-        })
-      )]
-    ).then(() => {
-      self.clients.claim()
-    })
-  )
-})
+// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
+const offlineFallbackPage = "404.html";
 
-// 请求接口事件，处理相关逻辑
-self.addEventListener('fetch', (e) => {
-  // console.log(e.request.url)
-  if (e.request.url.match('lcapi')) return
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request.url)))
-})
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
+});
+
+self.addEventListener('install', async (event) => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then((cache) => cache.add(offlineFallbackPage))
+  );
+});
+
+if (workbox.navigationPreload.isSupported()) {
+  workbox.navigationPreload.enable();
+}
+
+workbox.routing.registerRoute(
+  new RegExp('/*'),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: CACHE
+  })
+);
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.url.match('lcapi')) return
+  if (event.request.mode === 'navigate') {
+    event.respondWith((async () => {
+      try {
+        const preloadResp = await event.preloadResponse;
+
+        if (preloadResp) {
+          return preloadResp;
+        }
+
+        const networkResp = await fetch(event.request);
+        return networkResp;
+      } catch (error) {
+
+        const cache = await caches.open(CACHE);
+        const cachedResp = await cache.match(offlineFallbackPage);
+        return cachedResp;
+      }
+    })());
+  }
+});
